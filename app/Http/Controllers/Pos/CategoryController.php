556b1ed7b3101ff21cd8ce0b5a53ba89bf36b7bc
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2022-11-15 23:18:59 +0600

    Create function CategoryAdd()=>CategoryStore()=> & create views category_add.blade.php & create Route CategoryAdd=>CategoryStore

--- app/Http/Controllers/Pos/CategoryController.php
+++ app/Http/Controllers/Pos/CategoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Pos;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -15,4 +17,62 @@ class CategoryController extends Controller
          return view('backend.category.category_all',compact('categories'));
 
     }// End Method
+
+
+    public function CategoryAdd()
+    {
+        return view('backend.category.category_add');
+
+    }// End Method
+
+
+    //     public function CategoryStore(Request $request)
+    // {
+    //     Category::insert([
+    //         'name'=> $request->name,
+    //         'created_by'=> Auth::user()->id,
+    //         'created_at'=> Carbon::now(),
+    //     ]);
+
+    //     $notification = array(
+    //         'message'=>'Category  inserted successfully',
+    //         'alert-type'=>'success',
+    //     );
+
+    //     return redirect()->route('category.all')->with($notification);
+
+    // }// End Method by easylearningbd.com Kazi Ariyan
+
+
+    public function CategoryStore(Request $request)
+    {        
+        $data = new Category();
+        $data->name = $request->name;
+        $data->created_by = Auth::user()->id;
+        $data->created_at = Carbon::now();
+        $data->save();
+
+        
+        if($data)
+       {
+        $notification = array(
+            'message' => 'Category inserted Successfully', 
+            'alert-type' => 'success'
+        );
+    
+       }
+       else
+       {
+            $notification = array(
+                'message' => 'Category not inserted ', 
+                'alert-type' => 'error'
+            );
+       }// End if else BY m.k
+
+        return redirect()->route('category.all')->with($notification);
+
+    }// End Method BY Aman Sir
+
+
+
 }
