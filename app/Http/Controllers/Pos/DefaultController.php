4a33f3610b9bb3f936b1b335429424e16ab5ce10
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2022-11-23 23:39:45 +0600

    In purchase_add.blade.php add javascript On change event & Create Route 'get.product' & add function GetProduct() in DefaultController

--- app/Http/Controllers/Pos/DefaultController.php
+++ app/Http/Controllers/Pos/DefaultController.php
@@ -20,5 +20,17 @@ class DefaultController extends Controller
         $allCategory = Product::with(['category'])->select('category_id')->where('supplier_id',$supplier_id)->groupBy('category_id')->get();
         // dd($allCategory);
         return response()->json($allCategory);
+
+    } // End Method
+
+
+    public function GetProduct(Request $request)
+    {
+        $category_id = $request->category_id;
+      
+        $allProduct = Product::where('category_id',$category_id)->get();
+        
+        return response()->json($allProduct);
+
     } // End Method
 }
