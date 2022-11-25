f8f91e397f9537ce9f212df3c3d53b314fcbe876
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2022-11-26 01:04:15 +0600

    In purchase_add.blade.php Add plugin  Handlebars.compile() && add notify plugin to field validation && Create addeventmore section && Create removeeventmore section && Create invoice section buying_qty & buying_price & calculate sum as totalAmountPrice() for purchase invoice section

--- resources/views/backend/purchase/purchase_add.blade.php
+++ resources/views/backend/purchase/purchase_add.blade.php
@@ -180,13 +180,13 @@
     <script type="text/javascript">
         $(document).ready(function(){
             $(document).on("click",".addeventmore",function(){
-                var date = $(#date).val();
-                var purchase_no = $(#purchase_no).val();
-                var supplier_id = $(#supplier_id).val();
-                var category_id = $(#category_id).val();
-                var category_name = $(#category_id).find('option:selected').text();
-                var product_id = $(#product_id).val();
-                var product_name = $(#product_id).find('option:selected').text();
+                var date = $("#date").val();
+                var purchase_no = $("#purchase_no").val();
+                var supplier_id = $("#supplier_id").val();
+                var category_id = $("#category_id").val();
+                var category_name = $("#category_id").find('option:selected').text();
+                var product_id = $("#product_id").val();
+                var product_name = $("#product_id").find('option:selected').text();
                 
                 if(date == ''){
                     $.notify("Date is Required", {globalPosition: 'top right', className:'error' });
@@ -208,15 +208,56 @@
                     return false;
                 }
 
-                if(Product_id == ''){
+                if(product_id == ''){
                     $.notify("Product Field is Required", {globalPosition: 'top right', className:'error' });
                     return false;
                 }
 
-                var source = $("document-template").html();
-                var template = Handlebars.complie(source);
+
+                var source = $("#document-template").html(); //handlebars.js
+                var tamplate = Handlebars.compile(source);
+                var data = {
+                    date:date,
+                    purchase_no:purchase_no,
+                    supplier_id:supplier_id,
+                    category_id:category_id,
+                    category_name:category_name,
+                    product_id:product_id,
+                    product_name:product_name,
+
+                };
+
+                var html = tamplate(data);
+                $("#addRow").append(html);
 
             });
+
+            $(document).on("click",".removeeventmore",function(event){
+                $(this).closest(".delete_add_more_item").remove();
+                totalAmountPrice();
+            });
+
+            $(document).on("keyup click",".unit_price,.buying_qty",function(){
+                var unit_price = $(this).closest("tr").find("input.unit_price").val();
+                var qty = $(this).closest("tr").find("input.buying_qty").val();
+                var total = unit_price * qty;
+                $(this).closest("tr").find("input.buying_price").val(total);
+                totalAmountPrice();
+            });
+
+            // calculate sum of amount in invoice
+            function totalAmountPrice(){
+                var sum = 0;
+                $(".buying_price").each(function(){
+                    var value = $(this).val();
+                    if(!isNaN(value) && value.length != 0){
+                        sum += parseFloat(value);
+                    }
+                });
+
+                $("#estimated_amount").val(sum);
+            }
+
         });
     </script>
 
@@ -234,9 +275,9 @@
                   success:function(data){
                     var html = '<option value="">Select Category</option>';
                     $.each(data,function(key,v){
-                        html += '<option value=" '+v.category_id+' ">'+v.category.name+'</option>';
+                        html += '<option value="'+v.category_id+'">'+v.category.name+'</option>';
                     });
-                    $('#category_id').html(html);
+                    $("#category_id").html(html);
                   }
 
                 })
@@ -254,11 +295,11 @@
               type: "GET",
               data:{category_id:category_id},
               success:function(data){
-                var html = '<option value="">Select Category</option>';
+                var html = '<option value="">Select Product</option>';
                 $.each(data,function(key,v){
-                    html += '<option value=" '+v.id+' ">'+v.name+'</option>';
+                    html += '<option value="'+v.id+'">'+v.name+'</option>';
                 });
-                $('#product_id').html(html);
+                $("#product_id").html(html);
               }
 
             })
