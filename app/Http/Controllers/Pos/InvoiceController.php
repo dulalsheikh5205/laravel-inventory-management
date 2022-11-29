adb18de07f8ea65561115ddce051b9c0c79ff122
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2022-11-30 01:01:01 +0600

    In InvoiceController Create InvoiceAll() && views invoice_all.blade.php

--- app/Http/Controllers/Pos/InvoiceController.php
+++ app/Http/Controllers/Pos/InvoiceController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Pos;
 
-use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class InvoiceController extends Controller
 {
     public function InvoiceAll()
     {
-
+        $allData = Invoice::orderBy('date','desc')->orderBy('id','desc')->get();
+        return view('backend.invoice.invoice_all',compact('allData'));
+        
     } // End Method
 }
