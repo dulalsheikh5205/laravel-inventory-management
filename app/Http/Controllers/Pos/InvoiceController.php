db00f4db3dd83b75a766f7ab38c78b64bcdd20f6
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2022-11-30 08:09:31 +0600

    In InvoiceController.php Create invoiceAdd() && Create views invoice_add.blade.php & Route 'invoice.add'

--- app/Http/Controllers/Pos/InvoiceController.php
+++ app/Http/Controllers/Pos/InvoiceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Pos;
 
+use App\Models\Unit;
 use App\Models\Invoice;
+use App\Models\Category;
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,5 +16,17 @@ class InvoiceController extends Controller
         $allData = Invoice::orderBy('date','desc')->orderBy('id','desc')->get();
         return view('backend.invoice.invoice_all',compact('allData'));
         
+    } // End Method
+
+    public function InvoiceAdd()
+    {
+
+        $supplier = Supplier::all();
+        $unit = Unit::all();
+        $category = Category::all();
+
+        return view('backend.invoice.invoice_add',compact('supplier','unit','category'));
+
+
     } // End Method
 }
